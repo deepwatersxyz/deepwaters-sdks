cb31743621623cf28d9ca07e66bedd951ec5315f
Dom <dom@deepwaters.xyz>
2023-03-27 14:13:12 -0600

    more go examples

--- go/json.go
+++ go/json.go
@@ -1,18 +1,18 @@
 package main
 
 type ErrorResponse struct {
-	Code    uint16
+	Code    int
 	Error   string
 	Status  string
 	Success bool
 }
 
 type GetAPIKeySessionSuccessResponse struct {
-	Result  GetAPIKeySessionInner
+	Result  GetAPIKeySessionSuccessBody
 	Success bool
 }
 
-type GetAPIKeySessionInner struct {
+type GetAPIKeySessionSuccessBody struct {
 	APIKey           string
 	CreatedAtMicros  uint64
 	ExpiresAtMicros  uint64
@@ -20,4 +20,32 @@ type GetAPIKeySessionInner struct {
 	ModifiedAtMicros uint64
 	Nonce            uint64
 	Status           string
+}
+
+type SubmitOrderRequest struct {
+	CustomerObjectID *string `json:"customerObjectID,omitempty"`
+	Type             string  `json:"type"`
+	Side             string  `json:"side"`
+	QuantityStr      string  `json:"quantity"`
+	BaseAssetID      string  `json:"baseAssetID"`
+	QuoteAssetID     string  `json:"quoteAssetID"`
+	PriceStr         *string `json:"price,omitempty"`
+	DurationType     *string `json:"durationType,omitempty"`
+	ExpiresAtMicros  *uint64 `json:"expiresAtMicros,omitempty"`
+	ExpiresIn        *string `json:"expiresIn,omitempty"`
+}
+
+type SubmitOrderSuccessResponse struct {
+	Result  SubmitOrderSuccessBody
+	Success bool
+}
+
+type SubmitOrderSuccessBody struct {
+	CustomerObjectID  *string
+	ExpiresAtMicros   *uint64
+	OriginalQuantity  string
+	Quantity          string
+	RespondedAtMicros uint64
+	Status            string
+	VenueOrderID      string
 }
