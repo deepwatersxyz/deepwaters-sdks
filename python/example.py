014450c16ec779c8c8b67215440e4aacc7361c0c
Dom <dom@deepwaters.xyz>
2023-04-11 11:43:14 -0600

    refactor

--- python/example.py
+++ python/example.py
@@ -10,9 +10,14 @@ nonce_d = {'nonce': None}
 base_asset_id = 'WBTC.GOERLI.5.TESTNET.PROD'
 quote_asset_id = 'USDC.GOERLI.5.TESTNET.PROD'
 
+def get_request_uri_and_url_from_extension(host_url, api_route, extension):
+    request_uri = api_route + extension
+    url = host_url + request_uri
+    return request_uri, url
+
 def sync_nonce(api_key, api_secret, nonce_d):
     extension = 'customer/api-key-status'
-    request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+    request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
     headers = auth.get_authentication_headers(api_key, api_secret, 'GET', request_uri)
     r = requests.get(url, headers=headers)
     response = r.json()
@@ -25,7 +30,7 @@ sync_nonce(api_key, api_secret, nonce_d)
 # GET /customer
 
 extension = 'customer'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'GET', request_uri)
 
@@ -40,7 +45,7 @@ print()
 # GET /customer/api-key-status
 
 extension = 'customer/api-key-status'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'GET', request_uri)
 
@@ -52,7 +57,7 @@ print()
 
 # GET /pairs
 
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, 'pairs')
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, 'pairs')
 
 print('GET %s ... ' % url)
 r = requests.get(url)
@@ -64,7 +69,7 @@ print()
 
 pair_name = base_asset_id + '-' + quote_asset_id
 extension = f'pairs/{pair_name}'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 print('GET %s ... ' % url)
 r = requests.get(url)
@@ -75,7 +80,7 @@ print()
 # GET /pairs/{pair_name}/orderbook?depth={depth}
 
 extension = f'pairs/{pair_name}/orderbook?depth=12'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 print('GET %s ... ' % url)
 r = requests.get(url)
@@ -99,7 +104,7 @@ print()
 # for fetching with filtering later
 created_at_or_after_micros = auth.now_micros()
 
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, 'orders')
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, 'orders')
 
 payload = {"type": "LIMIT", "side": "BUY", "quantity": "1.00000", "price": "17000.00", "baseAssetID": base_asset_id, "quoteAssetID": quote_asset_id, "durationType": "GOOD_TILL_EXPIRY", "customerObjectID": auth.now_micros(), "expiresAtMicros": int(int(auth.now_micros()) + 10*1e6)}
 
@@ -116,7 +121,7 @@ print()
 # DELETE /orders?pair={pair}
 
 extension = f'orders?pair={pair_name}'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'DELETE', request_uri, nonce_d)
 
@@ -127,7 +132,7 @@ print(response)
 print()
 
 extension = 'orders'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'DELETE', request_uri, nonce_d)
 
@@ -144,7 +149,7 @@ print()
 created_before_micros = auth.now_micros()
 
 extension = f'orders?pair={pair_name}&type=LIMIT&side=BUY&status-in=ACTIVE-PARTIALLY_FILLED-FILLED&created-at-or-after-micros={created_at_or_after_micros}&created-before-micros={created_before_micros}&skip=0&limit=5'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'GET', request_uri)
 
@@ -159,7 +164,7 @@ print()
 # GET /trades?pair={pair}&type={type}&created-at-or-after-micros={created_at_or_after_micros}&created-before-micros={created_before_micros}&skip={skip}&limit={limit}
 
 extension = f'trades?pair={pair_name}&type=FILL&created-at-or-after-micros={created_at_or_after_micros}&created-before-micros={created_before_micros}&skip=0&limit=1'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'GET', request_uri)
 
@@ -176,7 +181,7 @@ print()
 # first create an order to GET and then DELETE
 
 extension = 'orders'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 payload = {'type': 'LIMIT', 'side': 'BUY',
 'quantity': '1.0000', 'price': '17000.00', 'baseAssetID': base_asset_id, 'quoteAssetID': quote_asset_id}
@@ -192,7 +197,7 @@ print()
 venue_order_id = response['result']['venueOrderID']
 
 extension = f'orders/by-venue-order-id/{venue_order_id}'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'GET', request_uri)
 
@@ -203,7 +208,7 @@ print(response)
 print()
 
 extension = f'orders/by-venue-order-id/{venue_order_id}'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'DELETE', request_uri, nonce_d)
 
@@ -220,7 +225,7 @@ print()
 # first create an order to GET and then DELETE
 
 extension = 'orders'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 customer_object_id = str(datetime.datetime.now().timestamp())
 
@@ -237,7 +242,7 @@ print(response)
 print()
 
 extension = f'orders/by-customer-object-id/{customer_object_id}'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'GET', request_uri)
 
@@ -248,7 +253,7 @@ print(response)
 print()
 
 extension = f'orders/by-customer-object-id/{customer_object_id}'
-request_uri, url = auth.get_request_uri_and_url_from_extension(host_url, api_route, extension)
+request_uri, url = get_request_uri_and_url_from_extension(host_url, api_route, extension)
 
 headers = auth.get_authentication_headers(api_key, api_secret, 'DELETE', request_uri, nonce_d)
 
